cac498788aa75221673fbaaa300f204c95c4c777
michal <torzewicz.michal@gmail.com>
2018-10-13 20:18:41 +0200

    Added maven support and tutorial test

---
+++ src/test/java/com/obiektowe/DataFrameTest.java
@@ -0,0 +1,15 @@
+package com.obiektowe;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DataFrameTest {
+
+    @Test
+    public void shouldInsertData() {
+        assertThat(true).isEqualTo(true);
+    }
+
+
+}
